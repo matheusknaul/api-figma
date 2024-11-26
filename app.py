6ccb6aca7f512201a0498eea286c2cd5ffaf3fa1
matheusknaul <contatomatheush0909@gmail.com>
2024-11-26 09:21:07 -0300

    git initial

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     from main import get_json
-    json = get_json("IQHFsrTcXpJsMY2M6aopyY", "figd_JnR3V2SivkMrY7nQNQuGinxkYx2H5pfivFR0Pp-H")
+    json = get_json()
     return jsonify(json)
 
 
